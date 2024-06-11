a27a19757cdab32aa5affc75a455b642fbc71ead
khoa nguyen dang <ndkhoa0704@gmail.com>
2024-06-11 17:08:12 +0700

    add cookie

--- src/security/security.py
+++ src/security/security.py
@@ -15,22 +15,46 @@ from . import crud
 from .dependency import verify_password
 from sqlalchemy.orm import Session
 import os
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 
-SECRET_KEY = os.environ['FASTAPI_SECRET_KEY']
+SECRET_KEY = os.environ["FASTAPI_SECRET_KEY"]
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-oauth2_scheme = OAuth2PasswordBearer(
+class OAuth2PasswordCookie(OAuth2PasswordBearer):
+    """OAuth2 password flow with token in a httpOnly cookie."""
+
+    def __init__(self, *args, token_name: str = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._token_name = token_name or "my-jwt-token"
+
+    @property
+    def token_name(self) -> str:
+        """Get the name of the token's cookie."""
+        return self._token_name
+
+    async def __call__(self, request: Request) -> str:
+        """Extract and return a JWT from the request cookies.
+
+        Raises:
+            HTTPException: 403 error if no token cookie is present.
+        """
+        token = request.cookies.get(self._token_name)
+        if not token:
+            raise HTTPException(status_code=403, detail="Not authenticated")
+        return token
+
+
+oauth2_scheme = OAuth2PasswordCookie(
     tokenUrl="token",
-    scopes={
-        'article:create': 'create article',
-        'article:read': 'read article'
-    }
+    scopes={"article:create": "create article", "article:read": "read article"},
 )
 
 router = APIRouter()
+
 
 def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user(db, username)
@@ -39,6 +63,7 @@ def authenticate_user(db: Session, username: str, password: str):
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -52,8 +77,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 async def get_current_user(
-    security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)],
-    db = Depends(get_db)
+    security_scopes: SecurityScopes,
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db=Depends(get_db),
 ):
     if security_scopes.scopes:
         authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
@@ -96,8 +122,7 @@ async def get_current_active_user(
 
 @router.post("/token")
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db = Depends(get_db)
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db=Depends(get_db)
 ) -> schemas.Token:
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
@@ -107,4 +132,8 @@ async def login_for_access_token(
         data={"sub": user.username, "scopes": form_data.scopes},
         expires_delta=access_token_expires,
     )
-    return schemas.Token(access_token=access_token, token_type="bearer")
+    responseObj = schemas.Token(access_token=access_token, token_type="bearer")
+
+    response = JSONResponse(responseObj.model_dump())
+    response.set_cookie(key=oauth2_scheme.token_name, value=access_token, httponly=True)
+    return response
