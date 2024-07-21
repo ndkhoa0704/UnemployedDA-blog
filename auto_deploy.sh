d7ec22e2acf2935fa6ddde33342076217808454b
Khoa Nguyen Dang <ndkhoa0704@gmail.com>
2024-07-21 20:05:57 +0700

    update

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -5,6 +5,8 @@ set -o allexport && source ~/.env && set +o allexport
 date +%d-%m-%y/%H:%M:%S
 cd $REPO_PATH
 
+chown -R $USER postgres-data
+
 echo "$REPO_PATH"
 
 if ! git pull | grep -q 'Already up to date.'
